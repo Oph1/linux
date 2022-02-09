7a023789990523c6918fdaf610cbf0b4d860fda7
Oph1 <99147185+Oph1@users.noreply.github.com>
2022-02-09 14:06:44 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,25 @@ sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo
 
 sudo apt install $(cat tools.list | tr "\n" " ") -y
 
+
+
+Projects/
+└── Acme Company
+    ├── EPT
+    │   ├── evidence
+    │   │   ├── credentials
+    │   │   ├── data
+    │   │   └── screenshots
+    │   ├── logs
+    │   ├── scans
+    │   ├── scope
+    │   └── tools
+    └── IPT
+        ├── evidence
+        │   ├── credentials
+        │   ├── data
+        │   └── screenshots
+        ├── logs
+        ├── scans
+        ├── scope
+        └── tools
